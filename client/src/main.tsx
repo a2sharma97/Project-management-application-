1df70b6cbcdd3fa70f3736a36e3bc8ffd7e310aa
Abhishek Sharma <a2sharma97@gmail.com>
2026-05-24 15:18:32 +0530

    fix: add delay to ToastContainer for improved user experience

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -7,6 +7,6 @@ import './index.css'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
       <App />
-      <ToastContainer/>
+      <ToastContainer delay={4000}/>
   </StrictMode>,
 )
